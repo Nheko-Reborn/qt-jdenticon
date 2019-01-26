00c89b56c9cf309475f33e10a119937ae596dcf6
redsky17 <joedonofry@gmail.com>
2019-01-26 18:02:47 +0000

    Add QT Plugin definitions for use in Qt apps.

---
+++ src/jdenticonplugin.cpp
@@ -0,0 +1,10 @@
+#include "jdenticonplugin.h"
+#include "identicon.h"
+
+QString
+JdenticonPlugin::generate(const QString &message, uint16_t size)
+{
+    QByteArray byteArr = QByteArray::fromStdString(message.toStdString());
+    Identicon identicon(byteArr, size);
+    return identicon.generateSvg(identicon, false);
+}
---
+++ src/jdenticonplugin.h
@@ -0,0 +1,18 @@
+#ifndef JDENTICONPLUGIN_H
+#define JDENTICONPLUGIN_H
+
+#include <QObject>
+#include <QtPlugin>
+#include "jdenticoninterface.h"
+
+class JdenticonPlugin : public QObject, JdenticonInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "redsky17.Qt.JdenticonInterface")
+    Q_INTERFACES(JdenticonInterface)
+
+public:
+    QString generate(const QString &message, uint16_t size) override;
+};
+
+#endif // JDENTICONPLUGIN_H

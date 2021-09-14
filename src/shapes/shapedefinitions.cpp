623a08a55ba94f55cabbbf36eab98f3d8273fe0a
Nicolas Werner <nicolas.werner@hotmail.de>
2021-09-14 02:29:40 +0200

    Fix crash on plugin load

--- src/shapes/shapedefinitions.cpp
+++ src/shapes/shapedefinitions.cpp
@@ -3,39 +3,36 @@
 #include <array>
 
 namespace shapes {
-static QList<void (*)(rendering::Renderer &, int, int)> outerShapes_{
-  &ShapeDefinitions::outerOne,
-  &ShapeDefinitions::outerTwo,
-  &ShapeDefinitions::outerThree,
-  &ShapeDefinitions::outerFour,
-};
-
-static QList<void (*)(rendering::Renderer &, int, int)> centerShapes_{
-  &ShapeDefinitions::centerOne,
-  &ShapeDefinitions::centerTwo,
-  &ShapeDefinitions::centerThree,
-  &ShapeDefinitions::centerFour,
-  &ShapeDefinitions::centerFive,
-  &ShapeDefinitions::centerSix,
-  &ShapeDefinitions::centerSeven,
-  &ShapeDefinitions::centerEight,
-  &ShapeDefinitions::centerNine,
-  &ShapeDefinitions::centerTen,
-  &ShapeDefinitions::centerEleven,
-  &ShapeDefinitions::centerTwelve,
-  &ShapeDefinitions::centerThirteen,
-  &ShapeDefinitions::centerFourteen,
-};
 
 QList<void (*)(rendering::Renderer &, int, int)>
 ShapeDefinitions::CenterShapes()
 {
-    return outerShapes_;
+    return QList<void (*)(rendering::Renderer &, int, int)>{
+      &ShapeDefinitions::centerOne,
+      &ShapeDefinitions::centerTwo,
+      &ShapeDefinitions::centerThree,
+      &ShapeDefinitions::centerFour,
+      &ShapeDefinitions::centerFive,
+      &ShapeDefinitions::centerSix,
+      &ShapeDefinitions::centerSeven,
+      &ShapeDefinitions::centerEight,
+      &ShapeDefinitions::centerNine,
+      &ShapeDefinitions::centerTen,
+      &ShapeDefinitions::centerEleven,
+      &ShapeDefinitions::centerTwelve,
+      &ShapeDefinitions::centerThirteen,
+      &ShapeDefinitions::centerFourteen,
+    };
 }
 
 QList<void (*)(rendering::Renderer &, int, int)>
 ShapeDefinitions::OuterShapes()
 {
-    return centerShapes_;
+    return QList<void (*)(rendering::Renderer &, int, int)>{
+      &ShapeDefinitions::outerOne,
+      &ShapeDefinitions::outerTwo,
+      &ShapeDefinitions::outerThree,
+      &ShapeDefinitions::outerFour,
+    };
 }
 }

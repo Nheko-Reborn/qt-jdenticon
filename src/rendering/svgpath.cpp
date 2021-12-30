c17265183a937cd226f9dbbb772a66dec2d41277
Nicolas Werner <nicolas.werner@hotmail.de>
2021-12-30 04:58:46 +0100

    Try fallback for systems without to_chars

--- src/rendering/svgpath.cpp
+++ src/rendering/svgpath.cpp
@@ -3,12 +3,45 @@
 #include <array>
 #include <charconv>
 
+namespace {
+// See http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4502.pdf.
+template<typename...>
+using void_t = void;
+
+// Primary template handles all types not supporting the operation.
+template<typename, template<typename> class, typename = void_t<>>
+struct detect : std::false_type
+{};
+
+// Specialization recognizes/validates only types supporting the archetype.
+template<typename T, template<typename> class Op>
+struct detect<T, Op, void_t<Op<T>>> : std::true_type
+{};
+
+// Archetypal expression for assignment operation.
+template<typename T>
+using to_chars_test = decltype(std::to_chars(std::declval<const char *>(),
+                                             std::declval<const char *>(),
+                                             std::declval<T>()));
+
+// Trait corresponding to that archetype.
+template<typename T>
+using is_to_chars_avaiable = detect<T, to_chars_test>;
+}
+
 static const auto formatNumber = [](auto number, std::array<char, 16> &buf) {
-    if (auto [ptr, ec] = std::to_chars(buf.data(), buf.data() + buf.size(), number);
-        ec == std::errc())
-        return QLatin1String(buf.data(), ptr);
-    else
-        return QLatin1String();
+    if constexpr (is_to_chars_avaiable<decltype(number)>()) {
+        if (auto [ptr, ec] = std::to_chars(buf.data(), buf.data() + buf.size(), number);
+            ec == std::errc())
+            return QLatin1String(buf.data(), ptr);
+        else
+            return QLatin1String();
+    } else {
+        if (auto sz = snprintf(buf.data(), buf.size(), "%f", number); sz > 0)
+            return QLatin1String(buf.data(), buf.data() + sz);
+        else
+            return QLatin1String();
+    }
 };
 
 namespace rendering {
